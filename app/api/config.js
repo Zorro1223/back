9b21c705302590df30bb497dd760d32a976cefa2
Zorro1223 <stremer12@gmail.com>
2024-05-29 18:38:27 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://pindieback.nomorepartiesco.ru/api'
+export const BASE_URL = 'https://pindieback.nomorepartiesco.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
